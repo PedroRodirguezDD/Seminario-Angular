fcd0da3cda5bcceb5742b17bde0cfda8ea622cdb
Pedro RDD <pedro.rodriguez.diaz.delfino.5@gmail.com>
2022-11-30 00:24:28 -0300

    detalle

--- src/app/cancion-data.service.ts
+++ src/app/cancion-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Song } from './songs-list/Song';
 
 const URL='https://63866d90beaa6458267c6c63.mockapi.io/Canciones';
@@ -13,7 +13,7 @@ export class CancionDataService {
   constructor(private http: HttpClient) { }
 
   public getAll(): Observable<Song[]>{
-    return this.http.get<Song[]>(URL).pipe(tap( (canciones:Song[]) => console.log(canciones)));
+    return this.http.get<Song[]>(URL);
   }
 
 }
